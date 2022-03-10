718167ee31eb0d16169fddea55b1fa6f7c7fb8d3
JENOUVRIER Hugo <jenohug@ndlp.fr>
2022-03-10 15:40:00 +0100

    Fix code

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -96,7 +96,6 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
         }
 
         return new RedirectResponse($this->urlGenerator->generate('home'));
-        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl()
